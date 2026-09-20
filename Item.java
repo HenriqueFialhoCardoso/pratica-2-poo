1231056747a3d9790412e0e9150e0211d0601ae1
Henrique Fialho Cardoso <2403henrique2005@gmail.com>
2026-09-20 15:10:44 -0300

    Criar Item.java

---
+++ Item.java
@@ -0,0 +1,32 @@
+public class Item {
+    private Produto produto;
+    private int quantidade;
+
+    public Item(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValorTotal() {
+        return produto.getPreco() * quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return produto.getNome()
+                + " | Quantidade: " + quantidade
+                + " | Valor: R$ " + String.format("%.2f", getValorTotal());
+    }
+}
